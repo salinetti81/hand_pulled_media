4940be716aabbaad6c0c8576d63ca7e27ebf023f
Daniel Salinetti <dansalinetti@gmail.com>
2016-08-18 09:54:35 -0400

    Adding ng-click event for director section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 var app = angular.module('HandPulledMedia', []);
 
-app.controller('BodyController', [function(){ //constructor function called MyController
-    this.hello = 'oh hai!';
+app.controller('BodyController', [function($scope,ngBody){ 
+    
+    this.displayContent = false;
 
-
+    // click to reveal content
+    var controller = this;
+    this.showContent = function() {
+      controller.displayContent = !controller.displayContent;
+    }
 }]);
 
 
