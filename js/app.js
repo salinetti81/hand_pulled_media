b6d49e84120f449abdaf65a210fe6328d1df8661
Daniel Salinetti <dansalinetti@gmail.com>
2016-08-18 12:58:37 -0400

    Adding a jQuery function to scroll when nav items are clicked

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+$(document).ready(function() {
+
+  $("#directors").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#section_2").offset().top},
+        'slow');
+  });
+
+  $("#about").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#section_3").offset().top},
+        'slow');
+  });
+
+  $("#contact").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#section_4").offset().top},
+        'slow');
+  });
+
+  });//ends document.ready
+
 var app = angular.module('HandPulledMedia', []);
 
 app.controller('BodyController', [function($scope,ngBody){ 
