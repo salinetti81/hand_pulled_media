d5d15a2347d20453e4feb1d19acb4d80f43bca5d
Daniel Salinetti <dansalinetti@gmail.com>
2016-08-26 12:15:11 -0400

    Changing section 1 to full screen on mobile and using javascript to get rid of hover events on mobile

--- js/app.js
+++ js/app.js
@@ -31,6 +31,26 @@ $(document).ready(function() {
   });
 
   });//ends document.ready
+var touch = window.ontouchstart
+            || (navigator.MaxTouchPoints > 0)
+            || (navigator.msMaxTouchPoints > 0);
+
+if (touch) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
 
 var app = angular.module('HandPulledMedia', []);
 
